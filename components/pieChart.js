2cdbad3f99c0573df22c1e2bdb7efd1f94820371
Alexandre <8884545+huijari@users.noreply.github.com>
2020-07-05 00:02:43 -0300

    implement pie chart

---
+++ components/pieChart.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Card } from 'antd'
+import { VictoryPie, VictoryLabel, VictoryLegend } from 'victory'
+
+const colorScale = ['#2ed47a', '#f7685b', '#ffb946']
+
+const PieChart = ({ title, data, x, y, total = '', bordered = true }) => {
+	return (
+		<Card title={title} bordered={bordered}>
+			<svg viewBox="0 0 650 400">
+				<VictoryLabel
+					textAnchor="middle"
+					style={{ fontSize: 25 }}
+					x={200}
+					y={200}
+					text={total}
+				/>
+				<VictoryPie
+					standalone={false}
+					width={400}
+					height={400}
+					data={data}
+					x={x}
+					y={y}
+					innerRadius={100}
+					colorScale={colorScale}
+					labels={[]}
+				/>
+				<VictoryLegend
+					standalone={false}
+					x={400}
+					y={50}
+					centerTitle
+					gutter={20}
+					data={data.map(item => ({ name: item[x] }))}
+					style={{ labels: { fontSize: 25 } }}
+					colorScale={colorScale}
+				/>
+			</svg>
+		</Card>
+	)
+}
+
+export default PieChart
