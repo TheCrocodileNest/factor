0c49803bf0edc010ce9ec7591082250e9d9a59b2
Francisco Dias <franciscopdneto@gmail.com>
2020-07-05 17:48:25 -0300

    Channel page with layout prototype defined.

--- pages/channels.js
+++ pages/channels.js
@@ -1,20 +1,34 @@
-import { Card } from 'antd'
+import { Card, Row, Col, Divider } from 'antd'
 
 import Navigation from '../components/navigation'
+import PieChart from '../components/pieChart'
 
-export default function Channels({ channels }) {
+export default function Channels({ channels, payments }) {
 	return (
 		<Navigation section="channels">
-			{channels.map(channel => (
-				<Card title={channel.Name} key={channel.Id}>
-					<div>
-						<h4>Produto em Destaque</h4>
-						<div>{channel.spotlightProduct}</div>
-					</div>
-					<div>
-						<h4>Categoria em Destaque</h4>
-						<div>{channel.spotlightCategory}</div>
-					</div>
+			{channels.map((channel) => (
+				<Card title={channel.Name} key={channel.Id} style={{ width: '100%', marginBottom: '30px' }}>
+				<Grid style={{width:'100%'}}>
+					<Row>
+						<Col flex={1} style={{marginRight: '20px'}}>
+							<Card title='Produto em Destaque' style={{marginBottom: '20px'}}>
+								{channel.spotlightProduct}
+							</Card>
+							<Card title='Categoria em Destaque'>
+								{channel.spotlightCategory}
+							</Card>
+						</Col>
+						<Col flex={4}>
+							<PieChart
+								title="Vendas por meio de pagamento"
+								total={payments[channel.Id].total}
+								x="name"
+								y="value"
+								data={payments[channel.Id].payment}
+							/>
+						</Col>
+					</Row>
+				</Grid>
 				</Card>
 			))}
 		</Navigation>
@@ -22,7 +36,13 @@ export default function Channels({ channels }) {
 }
 
 import getChannels from '../services/channels'
+import getPayments from '../services/payments'
+import Grid from 'antd/lib/card/Grid'
 export async function getStaticProps() {
 	const channels = getChannels()
-	return { props: { channels } }
+	const payments = {}
+	for (const channel of channels) {
+		payments[channel.Id] = getPayments({channel: channel.Id})
+	}
+	return { props: { channels, payments } }
 }
---
+++ services/payments.js
@@ -0,0 +1,36 @@
+import orders from '../data/orders.json'
+
+const getPayments = ({month, channel}) => {
+	let total = 0
+	const days = {}
+	const payments = {}
+
+	const ordersFromChannel = orders.filter(order => {
+		return order.salesChannel == channel
+	})
+	for (const order of ordersFromChannel) {
+		total += order.totalValue
+
+		if (days[order.creationDate] === undefined)
+			days[order.creationDate] = {
+				date: order.creationDate,
+				value: order.totalValue,
+			}
+		else days[order.creationDate].value += order.totalValue
+
+		if (payments[order.paymentNames] === undefined)
+			payments[order.paymentNames] = {
+				name: order.paymentNames,
+				value: order.totalValue,
+			}
+		else payments[order.paymentNames].value += order.totalValue
+	}
+
+	return {
+		month: Object.values(days),
+		payment: Object.values(payments),
+		total,
+	}
+}
+
+export default getPayments
