2a8421c6d8c49cbfca451f509afb3e5b8e058957
Alexandre <8884545+huijari@users.noreply.github.com>
2020-07-04 22:59:17 -0300

    generate orders

--- data/createData.js
+++ data/createData.js
@@ -1,3 +1,4 @@
+const { writeFileSync } = require('fs')
 const { v4: uuid } = require('uuid')
 
 if (process.argv.length !== 4) {
@@ -20,10 +21,10 @@ function randomChannel() {
 	const { Name, Payments } = channels[random(0, channels.length - 1)]
 	return {
 		Name,
-		Payment: Payments[random(0, payments.length - 1)],
+		Payment: Payments[random(0, Payments.length - 1)],
 	}
 }
-function randomOrders(creationDate, ordersDaily = [2, 4]) {
+function randomOrders(creationDate, ordersDaily = [10, 20]) {
 	return new Array(random(...ordersDaily)).fill(0).map(() => {
 		const items = randomItems()
 		const channel = randomChannel()
@@ -31,8 +32,8 @@ function randomOrders(creationDate, ordersDaily = [2, 4]) {
 			id: uuid(),
 			creationDate,
 			items,
-			salesChannel: channel.name,
-			paymentNames: channel.payment,
+			salesChannel: channel.Name,
+			paymentNames: channel.Payment,
 			totalItems: items.length,
 			totalValue: items.reduce((acc, { Price }) => acc + Price, 0),
 		}
@@ -47,4 +48,4 @@ for (let day = start; day <= end; day.setDate(day.getDate() + 1)) {
 	orders = [...orders, ...randomOrders(new Date(day))]
 }
 
-console.log(orders)
+writeFileSync('data/orders.json', JSON.stringify(orders, null, 2))
